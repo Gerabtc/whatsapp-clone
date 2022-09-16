f549765b83059c3f37fa259dd21deaddc982a7ac
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-16 19:20:28 -0300

    Configurando e implementando a escolha de fotos, criado dois vector asset na pasta drawable, camera e foto, e também criei os 2 ids, na classe ConfiguracoesActivity, adicionei dois atributos, imageButtonCamera e imageButtonGaleria, depois implementei os dois eventos de cliques onClickListener nos 2 botões, adicionei a tag feature no arquivo manifest para liberar o acesso a câmera externa, criei 2 constantes, selecao_camera e selecao_galeria, e adiocionei também o método onActivityResult, alteração do tamanho da imagem layout width e height no activity_configuracoes, voltando para a classe ConfiguracaoActivity inclui um atributo CircleImageView.

--- app/src/main/java/com/gerardusrocha/whatsappclone/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/activity/ConfiguracoesActivity.java
@@ -1,17 +1,26 @@
 package com.gerardusrocha.whatsappclone.activity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.gerardusrocha.whatsappclone.R;
 import com.gerardusrocha.whatsappclone.helper.Permissao;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
 
@@ -19,6 +28,10 @@ public class ConfiguracoesActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.CAMERA
     };
+    private ImageButton imageButtonCamera, imageButtonGaleria;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+    private CircleImageView circleImageViewPerfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +40,71 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         Permissao.validarPermissao(permissoesNecessarias, this, 1);
 
+        imageButtonCamera = findViewById(R.id.imageButtonCamera);
+        imageButtonGaleria = findViewById(R.id.imageButtonGaleria);
+        circleImageViewPerfil = findViewById(R.id.circleImageViewFotoPerfil);
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        imageButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_CAMERA);
+                }
+
+            }
+        });
+
+        imageButtonGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if (i.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Bitmap imagem = null;
+
+            try {
+                switch (requestCode) {
+                    case SELECAO_CAMERA:
+                        imagem = (Bitmap) data.getExtras().get("data");
+                        break;
+                    case SELECAO_GALERIA:
+                        Uri localImagemSelecionada = data.getData();
+                        imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+                        break;
+                }
+
+                if (imagem != null) {
+
+                    circleImageViewPerfil.setImageBitmap(imagem);
+
+                }
+
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
 
     }
 
