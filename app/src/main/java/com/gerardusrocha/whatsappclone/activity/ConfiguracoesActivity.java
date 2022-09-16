d20d9c62d4693fc6c4c875c8e061a32e20f58c98
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-16 13:20:58 -0300

    Configurando as permissões no arquivo manifest, adicionado  a uses_permission read_external_storage e camera, que permite o acesso ao armazenamento externo e a câmera, e implementado o atributo array de string permissoesNecessarias e adicionado a permissao no método onCreate no arquivo ConfiguracoesActivity, depois criei o método onRequestPermissionResult e o método alertaValidacaoPermissao  com um alertDialog, depois foi criado uma classe Permissao no pacote helper, e adicionado um método validarPermissoes.

--- app/src/main/java/com/gerardusrocha/whatsappclone/activity/ConfiguracoesActivity.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/activity/ConfiguracoesActivity.java
@@ -1,18 +1,31 @@
 package com.gerardusrocha.whatsappclone.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.gerardusrocha.whatsappclone.R;
+import com.gerardusrocha.whatsappclone.helper.Permissao;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+
+    private String[] permissoesNecessarias = new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracoes);
+
+        Permissao.validarPermissao(permissoesNecessarias, this, 1);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
@@ -20,7 +33,35 @@ public class ConfiguracoesActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults){
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                alertaValidarPermissao();
+            }
+        }
+
+    }
+
+    private void alertaValidarPermissao() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões negadas!");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 }
