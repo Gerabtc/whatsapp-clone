51aa23298e7088ae97691d80fdf71f74b9e36f38
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-17 17:36:50 -0300

    Configurando a lista de contatos, adicionado um recyclerView na fragment_contatos, e criado o id recycler ViewListaContatos, criado o atributo na classe ContatosFragment e modificado o método onCreateView, depois é criado um pacote adapter e uma classe dentro dele, ContatosAdapter extendendo RecyclerView.Adapter e implementado o método MyViewHolder, e os métodos onCreateViewHolder, onBindViewHolder e getItemCount, na classe ContatosFragment criei um atributo, ContatosAdapter e um arrayList listaContatos, na classe ContatosAdaper criei um atributo List de Usuário e um Context e um construtor contatosAdapter que recebe como parâmetro a List de Usuário, agora é configurar o adapter na classe ContatosFragment, configurando agora a exibição dos itens na classe ContatosAdapter, criado um arquivo na pasta layout adapter_contatos, e inclui um circleImageView, um linearLayout vertical, um textView nome e email e seus respectivos ids.

--- app/src/main/java/com/gerardusrocha/whatsappclone/fragment/ContatosFragment.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/fragment/ContatosFragment.java
@@ -3,12 +3,18 @@ package com.gerardusrocha.whatsappclone.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.gerardusrocha.whatsappclone.R;
+import com.gerardusrocha.whatsappclone.adapter.ContatosAdapter;
+import com.gerardusrocha.whatsappclone.model.Usuario;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +22,10 @@ import com.gerardusrocha.whatsappclone.R;
  * create an instance of this fragment.
  */
 public class ContatosFragment extends Fragment {
+
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<Usuario> listaContatos = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +71,17 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
     }
 }
