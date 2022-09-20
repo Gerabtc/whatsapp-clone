56427716c7635bcc6ac3895282f97c740c40a52e
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-20 19:39:23 -0300

    Configurar destinatário no chat, configurar o layout adapter_contatos, criar o arquivo divisor no layout e incluir no arquivo adapter_activity, implementar os atributos textViewTitulo e circleImageViewFoto na classe ChatActivity, incluir atributo usuarioSelecionado no método onCreateView da classe ContatosFragment, implementar a interface serializable na classe Usuario, passar objetos entre uma activity e outra, incluir um atributo Bundle e usuarioDestinatario na classe ChatActivity

--- app/src/main/java/com/gerardusrocha/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/activity/ChatActivity.java
@@ -3,14 +3,24 @@ package com.gerardusrocha.whatsappclone.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.gerardusrocha.whatsappclone.R;
+import com.gerardusrocha.whatsappclone.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ChatActivity extends AppCompatActivity {
+
+    private TextView textViewNome;
+    private CircleImageView circleImageViewFoto;
+    private Usuario usuarioDestinatario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,26 @@ public class ChatActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        textViewNome = findViewById(R.id.textViewNomeChat);
+        circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
 
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+
+            usuarioDestinatario = (Usuario) bundle.getSerializable("chatContato");
+            textViewNome.setText(usuarioDestinatario.getNome());
+
+            String foto = usuarioDestinatario.getFoto();
+            if (foto != null) {
+                Uri url = Uri.parse(usuarioDestinatario.getFoto());
+                Glide.with(ChatActivity.this)
+                        .load(url)
+                        .into(circleImageViewFoto);
+            }else {
+                circleImageViewFoto.setImageResource(R.drawable.padrao);
+            }
+
+        }
 
     }
 }
