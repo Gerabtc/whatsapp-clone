4c8f59e16e3573b6a99fc75999ca61a7615f30ac
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-20 22:32:05 -0300

    Corrigir a formatação na content_activity e implementar o evento de onClick com o id do fab e do editTexto, criar o atributo editMensagem na classe ChatActivity, criar e implementar o método enviarMensagem, criar uma e implementar a classe Mensagem na pasta model com seus getter e setter e o construtor vazio, na classe ChatActivity criar os atributos, idUsuarioRemetente e idUsuarioDestinatario, e implementar o método salvarMensagem

--- app/src/main/java/com/gerardusrocha/whatsappclone/activity/ChatActivity.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/activity/ChatActivity.java
@@ -6,13 +6,20 @@ import androidx.appcompat.widget.Toolbar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.gerardusrocha.whatsappclone.R;
+import com.gerardusrocha.whatsappclone.config.ConfiguracaoFirebase;
+import com.gerardusrocha.whatsappclone.helper.Base64Custom;
+import com.gerardusrocha.whatsappclone.helper.UsuarioFirebase;
+import com.gerardusrocha.whatsappclone.model.Mensagem;
 import com.gerardusrocha.whatsappclone.model.Usuario;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -20,7 +27,11 @@ public class ChatActivity extends AppCompatActivity {
 
     private TextView textViewNome;
     private CircleImageView circleImageViewFoto;
+    private EditText editMensagem;
     private Usuario usuarioDestinatario;
+    private String idUsuarioRemetente;
+    private String idUsuarioDestinatario;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +46,9 @@ public class ChatActivity extends AppCompatActivity {
 
         textViewNome = findViewById(R.id.textViewNomeChat);
         circleImageViewFoto = findViewById(R.id.circleImageFotoChat);
+        editMensagem = findViewById(R.id.editMensagem);
+
+        idUsuarioRemetente = UsuarioFirebase.getIdentificadorUsuario();
 
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
@@ -52,7 +66,44 @@ public class ChatActivity extends AppCompatActivity {
                 circleImageViewFoto.setImageResource(R.drawable.padrao);
             }
 
+            idUsuarioDestinatario = Base64Custom.codificarBase64(usuarioDestinatario.getEmail());
+
         }
+
+    }
+
+    public void enviarMensagem(View view) {
+
+        String textoMensagem = editMensagem.getText().toString();
+
+        if (!textoMensagem.isEmpty()) {
+
+            Mensagem mensagem = new Mensagem();
+            mensagem.setIdUsuario(idUsuarioRemetente);
+            mensagem.setMensagem(textoMensagem);
+
+            salvarMensagem(idUsuarioRemetente, idUsuarioDestinatario, mensagem);
+
+
+        }else {
+            Toast.makeText(ChatActivity.this,
+                    "Digite uma mensagem para enviar!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void salvarMensagem(String idRemetente, String idDestinatario, Mensagem msg) {
+
+        DatabaseReference database = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference mensagemRef = database.child("mensagens");
+
+        mensagemRef.child(idRemetente)
+                .child(idDestinatario)
+                .push()
+                .setValue(msg);
+
+        editMensagem.setText("");
 
     }
 }
