7a0cf4c40929b956488d26e6ab3b5b7bd4f3524c
Gerabtc <rochagerardus1977@hotmail.com>
2022-09-21 22:21:32 -0300

    Abrir chat clicando na conversa, adicionar implementar o método addOnItemTouchListener

--- app/src/main/java/com/gerardusrocha/whatsappclone/fragment/ConversasFragment.java
+++ app/src/main/java/com/gerardusrocha/whatsappclone/fragment/ConversasFragment.java
@@ -1,5 +1,6 @@
 package com.gerardusrocha.whatsappclone.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,12 +12,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 import com.gerardusrocha.whatsappclone.R;
+import com.gerardusrocha.whatsappclone.activity.ChatActivity;
 import com.gerardusrocha.whatsappclone.adapter.ConversasAdapter;
 import com.gerardusrocha.whatsappclone.config.ConfiguracaoFirebase;
+import com.gerardusrocha.whatsappclone.helper.RecyclerItemClickListener;
 import com.gerardusrocha.whatsappclone.helper.UsuarioFirebase;
 import com.gerardusrocha.whatsappclone.model.Conversa;
+import com.gerardusrocha.whatsappclone.model.Usuario;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,11 +30,6 @@ import com.google.firebase.database.DatabaseReference;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ConversasFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ConversasFragment extends Fragment {
 
     private RecyclerView recyclerViewListaConversas;
@@ -39,44 +39,7 @@ public class ConversasFragment extends Fragment {
     private DatabaseReference conversasRef;
     private ChildEventListener childEventListenerConversas;
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public ConversasFragment() {}
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ConversasFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ConversasFragment newInstance(String param1, String param2) {
-        ConversasFragment fragment = new ConversasFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -92,6 +55,34 @@ public class ConversasFragment extends Fragment {
         recyclerViewListaConversas.setLayoutManager(layoutManager);
         recyclerViewListaConversas.setHasFixedSize(true);
         recyclerViewListaConversas.setAdapter(adapter);
+
+        recyclerViewListaConversas.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerViewListaConversas,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                                Conversa conversaSelecionada = listaConversas.get(position);
+
+                                Intent i = new Intent(getActivity(), ChatActivity.class);
+                                i.putExtra("chatContato", conversaSelecionada.getUsuarioExibicao());
+                                startActivity(i);
+
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                ));
 
         String identificadorUsuario = UsuarioFirebase.getIdentificadorUsuario();
         database = ConfiguracaoFirebase.getFirebaseDatabase();
